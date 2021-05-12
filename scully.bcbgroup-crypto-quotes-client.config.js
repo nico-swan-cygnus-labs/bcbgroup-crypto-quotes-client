a2bbd386569ac5d7c7b5c8c135e3916b5b85e2a7
Nico Swan <hi@nicoswan.com>
2021-05-12 12:53:19 +0200

    update static env

--- scully.bcbgroup-crypto-quotes-client.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    projectRoot: "./src",
-    projectName: "bcbgroup-crypto-quotes-client",
-    outDir: './dist/static',
-    routes: {}
-};
